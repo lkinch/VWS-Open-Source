d0e0071fc1e2bb2e78816020fa158b735a5721a2
CodeItQuick <evanontario@hotmail.com>
2021-01-02 15:18:45 -0400

    fix: Changed to version 4; Added IonAuth

---
+++ myapp/app/Config/IonAuth.php
@@ -0,0 +1,10 @@
+<?php namespace Config;
+
+class IonAuth extends \IonAuth\Config\IonAuth
+{
+    // set your specific config
+    public $siteTitle                = 'Virtual Wellness System OS';       // Site Title, example.com
+    // public $adminEmail               = 'admin@example.com'; // Admin Email, admin@example.com
+    // public $emailTemplates           = 'App\\Views\\auth\\email\\';
+    // ...
+}
