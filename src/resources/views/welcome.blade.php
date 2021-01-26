d5cf6975640a297b76f5fa1787ff10649279597b
Liam Kinch <lkinch@upei.ca>
2021-01-26 12:05:51 -0400

    Started creating a Contact Us page for the VWS

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -2,76 +2,39 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- CSS ORDER MATTERS -->
-	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-	<link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
-	<link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
-    <link rel="stylesheet" href="css/style.css" />
-    <title>Sticky Navigation</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
 </head>
 
 <body>
-	<div id="app">
-		<v-app>
-			<v-main>
-				<div>
-					<v-toolbar dense id="toolbar">
-						<v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-						<v-toolbar-title>Virtual Wellness System</v-toolbar-title>
-
-						<v-spacer></v-spacer>
-
-						<v-btn icon>
-							<v-icon>mdi-magnify</v-icon>
-						</v-btn>
-
-						<v-btn icon>
-							<v-icon>mdi-heart</v-icon>
-						</v-btn>
-
-						<v-btn icon>
-							<v-icon>mdi-dots-vertical</v-icon>
-						</v-btn>
-					</v-toolbar>
-					<v-carousel hide-delimiters>
-						<v-carousel-item
-						v-for="(item,i) in items"
-						:key="i"
-						:src="item.src"
-						></v-carousel-item>
-					</v-carousel>
-				</div>
-				<div>
-					<v-container class="grey lighten-5">
-						<h2>Content One</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem placeat quam nostrum itaque ut cum quasi at nam quas repellat, earum rem consectetur architecto sed iure, obcaecati ab veniam, laudantium illo. Autem ullam similique praesentium fuga sed quae cupiditate, minima corporis molestias quam, labore soluta et error veniam nihil tempore a officia laudantium minus voluptatibus aut, consequuntur sunt quis! Quae numquam sit possimus aperiam. Maxime libero numquam molestias, aliquam aut reprehenderit corporis ratione eaque maiores deserunt earum cupiditate distinctio explicabo non dolorum totam ipsam magni adipisci minus veritatis fuga. Dignissimos, magnam exercitationem reiciendis odit quibusdam sint quia fugit ducimus eum.
-						</p>
-					</v-container>
-					<v-container class="grey lighten-5">
-						<h3>Content Two</h3>
-						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa, ab! Voluptate eum repellendus repellat incidunt a enim, vero neque, cum reprehenderit, sunt assumenda eaque dignissimos atque eveniet unde. Nulla quam suscipit magni obcaecati est architecto quod maiores illum distinctio. Fuga vel, vitae nobis in at mollitia amet impedit, voluptas dolores, rerum cum assumenda sint asperiores temporibus. Quo, dicta dolor vitae harum labore culpa sunt voluptatum laudantium recusandae, ipsam iste. Eos, et. Aut aliquid earum, quas blanditiis expedita in quisquam facilis dolor reiciendis obcaecati, quo voluptatum libero, atque ut molestiae aspernatur minima et debitis totam provident eius harum. Quaerat, praesentium doloribus?</p>
-					</v-container>
-				</div>
-			</v-main>
-		</v-app>
-	</div>
-
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
-	<script type="module" src="/js/welcome_message.js">
-
-	</script>
-	<style >
-
-	</style>
+<h1>Contact Us</h1>
+<p></p>
+<p>At the Clinic of Dr. Montelpare, we wish to help everyone who seeks help.</p>
+<p></p>
+<p>As such, we would like to extend an invitation for anyone who wishes to meet with us either at our address:</p>
+<p></p>
+<b>11 Mafeking Parade<br>
+Hammersmith, London, UK<br>
+444-4444</b>
+<p></p>
+<p>Or by emailing us:</p>
+<p></p>
+<div class="input-group input-group-sm mb-3">
+  <span class="input-group-text" id="inputGroup-sizing-sm">Name</span>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+</div>
+<p></p>
+<div class="input-group input-group-sm mb-3">
+  <span class="input-group-text" id="inputGroup-sizing-sm">Email</span>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+</div>
+<p></p>
+<div class="input-group">
+  <span class="input-group-text">Message</span>
+  <textarea class="form-control" aria-label="With textarea"></textarea>
+</div>
+<p></p>
+<button type="button" class="btn btn-primary">Send</button>
 </body>
 
 </html>
