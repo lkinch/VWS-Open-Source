a0076940d5ff2458339746261d2dd24983840c69
CodeItQuick <evanontario@hotmail.com>
2021-02-13 14:08:01 -0400

    feat: likert scale works

--- src/routes/web.php
+++ src/routes/web.php
@@ -31,6 +31,14 @@ Route::get('/dashboard/surveys/appendices/surveyAppendixO', function(){
     return view('/dashboard/surveys/appendices/surveyAppendixO');
 })->name('surveyAppendixO');
 
+Route::get('/surveyAppendixQ', function(){
+    return view('/surveyAppendixQ');
+})->name('surveyAppendixQ');
+
+Route::get('/dashboard/surveylk', function(){
+    return view('/dashboard/surveylk');
+})->name('surveylk');
+
 Route::get('/dashboard/sampleSurvey', [SurveyController::class, 'index'])->name('SampleSurveyindex');
 Route::post('/dashboard/sampleSurvey', [SurveyController::class, 'store'])->name('SampleSurveystore');
 
