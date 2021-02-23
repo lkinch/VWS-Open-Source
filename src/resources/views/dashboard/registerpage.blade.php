ef0bb394e5ed1bf88af153e1ee8c21ec4a878f23
Liam Kinch <lkinch@upei.ca>
2021-02-23 10:09:07 -0400

    feat: created login/register pages

---
+++ src/resources/views/dashboard/registerpage.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- CSS ORDER MATTERS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/dashboard/index.css" />
+    <link rel="stylesheet" href="/css/dashboard/sidebar.css" />
+	<link rel="stylesheet" href="/css/dashboard/surveyrightbar.css" />
+
+	<script src="https://kit.fontawesome.com/555936ed9c.js" crossorigin="anonymous"></script>
+    <title>Register</title>
+
+</head>
+
+<body>
+    <div class="main">
+        @section('leftsidebar')
+            @include('dashboard.leftsidebar')
+        @show
+
+        <section class="right-panel" >
+            <div id="register">
+                @section('register')
+                    @include('auth.register')
+                @show
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
